92640a6d00fd3e3ae76c9261dc39ba7916653571
Aleksandr Razumov <ar@gortc.io>
2020-11-23 19:42:47 +0300

    feat(crypto): add NewMessageID

---
+++ internal/crypto/message_id.go
@@ -0,0 +1,97 @@
+package crypto
+
+import (
+	"time"
+)
+
+// Message identifiers are coupled to message creation time.
+//
+// https://core.telegram.org/mtproto/description#message-identifier-msg-id
+
+const (
+	yieldClient         = 0
+	yieldServerResponse = 1
+	yieldFromServer     = 3
+
+	messageIDModulo = 4
+)
+
+func newMessageID(now time.Time, yield int) int64 {
+	const nano = 1e9
+	// Must approximately equal unixtime*2^32.
+	nowNano := now.UnixNano()
+
+	// Important: to counter replay-attacks the lower 32 bits of msg_id
+	// passed by the client must not be empty and must present a
+	// fractional part of the time point when the message was created.
+	intPart := nowNano / nano
+	fracPart := nowNano % nano
+
+	// Ensure that fracPart % 4 == 0.
+	fracPart &= -messageIDModulo
+	// Adding modulo 4 yield to ensure message type.
+	fracPart += int64(yield)
+
+	return (intPart << 32) | fracPart
+}
+
+// MessageID represents 64-bit message id.
+type MessageID int64
+
+// MessageType is type of message determined by message id.
+//
+// A message is rejected over 300 seconds after it is created or
+// 30 seconds before it is created (this is needed to protect from replay attacks).
+//
+// The identifier of a message container must be strictly greater than those of
+// its nested messages.
+type MessageType byte
+
+const (
+	// MessageUnknown reports that message id has unknown time and probably
+	// should be ignored.
+	MessageUnknown = iota
+	// MessageFromClient is client message identifiers.
+	MessageFromClient
+	// MessageServerResponse is a response to a client message.
+	MessageServerResponse
+	// MessageFromServer is a message from the server.
+	MessageFromServer
+)
+
+// Time returns approximate time when MessageID were generated.
+func (id MessageID) Time() time.Time {
+	intPart := int64(id) >> 32
+	fracPart := int64(int32(id))
+	return time.Unix(intPart, fracPart)
+}
+
+// Type returns message type.
+func (id MessageID) Type() MessageType {
+	switch id % messageIDModulo {
+	case yieldClient:
+		return MessageFromClient
+	case yieldServerResponse:
+		return MessageServerResponse
+	case yieldFromServer:
+		return MessageFromServer
+	default:
+		return MessageUnknown
+	}
+}
+
+// NewMessageID returns new message id for provided time and type.
+func NewMessageID(now time.Time, typ MessageID) MessageID {
+	var modulo int
+	switch typ {
+	case MessageFromClient:
+		modulo = yieldClient
+	case MessageFromServer:
+		modulo = yieldFromServer
+	case MessageServerResponse:
+		modulo = yieldServerResponse
+	default:
+		modulo = yieldClient
+	}
+	return MessageID(newMessageID(now, modulo))
+}
---
+++ internal/crypto/message_id_test.go
@@ -0,0 +1,43 @@
+package crypto
+
+import (
+	"testing"
+	"time"
+)
+
+func TestMessageID(t *testing.T) {
+	now := time.Date(2018, 10, 10, 23, 42, 6, 13600, time.UTC)
+	id := MessageID(newMessageID(now, 0))
+	if id.Type() != MessageFromClient {
+		t.Fatal("invalid type")
+	}
+	if id != 6610877768685073696 {
+		t.Error("mismatch")
+	}
+	delta := id.Time().Sub(now)
+	if delta < 0 {
+		delta *= -1
+	}
+	if delta > time.Second {
+		t.Fatal("unexpected time drift")
+	}
+	t.Run("NewMessageID", func(t *testing.T) {
+		if NewMessageID(now, MessageFromServer).Type() != MessageFromServer {
+			t.Error("Mismatch")
+		}
+		if NewMessageID(now, 100).Type() != MessageFromClient {
+			t.Error("Mismatch")
+		}
+	})
+}
+
+func BenchmarkNewMessageID(b *testing.B) {
+	// Note that most overhead will be from time.Now() calls.
+	// Just ensuring that NewMessageID itself is reasonably fast.
+	now := time.Date(2018, 10, 10, 23, 42, 6, 13600, time.UTC)
+	for i := 0; i < b.N; i++ {
+		if NewMessageID(now, MessageFromServer).Type() != MessageFromServer {
+			b.Fatal("Mismatch")
+		}
+	}
+}
