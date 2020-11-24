ea8c237c13b135be3c26912f662250e288ef0ffd
Aleksandr Razumov <ar@gortc.io>
2020-11-24 21:19:45 +0300

    feat(proto): add {Read,Write}Intermediate

--- internal/proto/intermediate.go
+++ internal/proto/intermediate.go
@@ -1,6 +1,11 @@
 package proto
 
-import "github.com/ernado/td/bin"
+import (
+	"fmt"
+	"io"
+
+	"github.com/ernado/td/bin"
+)
 
 // The Intermediate version of MTproto.
 //
@@ -11,8 +16,34 @@ import "github.com/ernado/td/bin"
 // Note that server does not respond with it.
 var IntermediateClientStart = []byte{0xee, 0xee, 0xee, 0xee}
 
-// EncodeIntermediate encodes payload to b via Intermediate protocol.
-func EncodeIntermediate(b *bin.Buffer, payload []byte) {
-	b.PutInt32(int32(len(payload)))
-	b.Put(payload)
+// EncodeIntermediate encodes b as payload to w.
+func WriteIntermediate(w io.Writer, b *bin.Buffer) error {
+	// Re-using b.Buf if possible to reduce allocations.
+	buf := append(b.Buf[len(b.Buf):], make([]byte, 0, 4)...)
+	inner := bin.Buffer{Buf: buf}
+	inner.PutInt(b.Len())
+	if _, err := w.Write(inner.Buf); err != nil {
+		return err
+	}
+	if _, err := w.Write(b.Raw()); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ReadIntermediate reads payload from r to b.
+func ReadIntermediate(r io.Reader, b *bin.Buffer) error {
+	b.Buf = append(b.Buf[:0], make([]byte, 4)...)
+	if _, err := io.ReadFull(r, b.Buf[:4]); err != nil {
+		return fmt.Errorf("failed to read length: %w", err)
+	}
+	dataLen, err := b.Int32()
+	if err != nil {
+		return err
+	}
+	b.Buf = append(b.Buf[:0], make([]byte, int(dataLen))...)
+	if _, err := r.Read(b.Buf); err != nil {
+		return fmt.Errorf("failed to read payload: %w", err)
+	}
+	return nil
 }
---
+++ internal/proto/intermediate_test.go
@@ -0,0 +1,50 @@
+package proto
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/ernado/td/bin"
+)
+
+func BenchmarkWriteIntermediate(b *testing.B) {
+	out := new(bytes.Buffer)
+	buf := new(bin.Buffer)
+	buf.PutString("Hello world")
+	buf.PutString("Wake up")
+	buf.PutString("Neo")
+
+	b.ReportAllocs()
+	b.SetBytes(int64(buf.Len() + 4))
+
+	for i := 0; i < b.N; i++ {
+		if err := WriteIntermediate(out, buf); err != nil {
+			b.Fatal(err)
+		}
+		out.Reset()
+	}
+}
+
+func BenchmarkReadIntermediate(b *testing.B) {
+	out := new(bytes.Buffer)
+	buf := new(bin.Buffer)
+	buf.PutString("Hello world")
+	buf.PutString("Wake up")
+	buf.PutString("Neo")
+	if err := WriteIntermediate(out, buf); err != nil {
+		b.Fatal(err)
+	}
+	raw := out.Bytes()
+	reader := bytes.NewReader(nil)
+
+	b.ReportAllocs()
+	b.SetBytes(int64(buf.Len() + 4))
+
+	for i := 0; i < b.N; i++ {
+		reader.Reset(raw)
+		if err := ReadIntermediate(reader, buf); err != nil {
+			b.Fatal(err)
+		}
+		buf.Reset()
+	}
+}
