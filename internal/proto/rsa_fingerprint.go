40e7e161549ec923ab8fc5968d2ed57e629d2ad5
Aleksandr Razumov <ar@gortc.io>
2020-11-24 23:43:44 +0300

    feat(proto): RSAFingerprint

---
+++ internal/proto/rsa_fingerprint.go
@@ -0,0 +1,27 @@
+package proto
+
+import (
+	"crypto/rsa"
+	"crypto/sha1"
+	"encoding/binary"
+	"math/big"
+
+	"github.com/ernado/td/bin"
+)
+
+// RSAFingerprint returns fingerprint of RSA public key as defined in MTProto.
+func RSAFingerprint(key *rsa.PublicKey) int64 {
+	e := big.NewInt(int64(key.E))
+
+	// See "Creating an Authorization Key" for reference:
+	// * https://core.telegram.org/mtproto/auth_key#dh-exchange-initiation
+	// rsa_public_key n:string e:string = RSAPublicKey
+	buf := new(bin.Buffer)
+	buf.PutBytes(key.N.Bytes())
+	buf.PutBytes(e.Bytes())
+
+	h := sha1.New()
+	_, _ = h.Write(buf.Buf)
+	result := h.Sum(nil)[12:sha1.Size]
+	return int64(binary.LittleEndian.Uint64(result))
+}
---
+++ internal/proto/rsa_fingerpting_test.go
@@ -0,0 +1,26 @@
+package proto
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+)
+
+func TestRSAFingerprint(t *testing.T) {
+	testKey := []byte(`-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCJhOrkbrOi7/fFQRN2+8W5+Inx
+bdxo7XH5DDKgFvXPDDe8cQINO3/kFat7KlpC2n2sS8ApZQkmREANg0XpImL9lCHB
+v1FgQmL0xtnaURKo7FzaoaL4jCf5556NQr1th9F3oeN67mR4+BF0vPP9Gu6GY5Z1
+BSqi+FEREW/2aWSgSwIDAQAB
+-----END PUBLIC KEY-----`)
+	b, _ := pem.Decode(testKey)
+	keyParsed, err := x509.ParsePKIXPublicKey(b.Bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	key := keyParsed.(*rsa.PublicKey)
+	if RSAFingerprint(key) != 1914007313702140277 {
+		t.Fatal(RSAFingerprint(key))
+	}
+}
