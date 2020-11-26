0356767ad1b10760f1c127da1a2896c6f5aa5aae
Aleksandr Razumov <ar@gortc.io>
2020-11-27 00:07:49 +0300

    feat(bin): add Buffer.Copy

--- bin/buffer.go
+++ bin/buffer.go
@@ -7,6 +7,10 @@ type Buffer struct {
 	Buf []byte
 }
 
+func (b *Buffer) Copy() []byte {
+	return append([]byte{}, b.Buf...)
+}
+
 func (b *Buffer) Write(p []byte) (n int, err error) {
 	b.Buf = append(b.Buf, p...)
 	return len(p), nil
