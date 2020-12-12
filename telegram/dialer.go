efee5453535ca79958b055f7e3cfa96d7567bd56
tdakkota <tanc13@yandex.ru>
2020-12-12 17:33:46 +0300

    feat(cmd): update echo bot

--- telegram/dialer.go
+++ telegram/dialer.go
@@ -8,3 +8,9 @@ import (
 type Dialer interface {
 	DialContext(ctx context.Context, network, address string) (net.Conn, error)
 }
+
+type DialFunc func(ctx context.Context, network, address string) (net.Conn, error)
+
+func (d DialFunc) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	return d(ctx, network, address)
+}
