18e72827536106693636e6d149d08b8009cbf055
tdakkota <tanc13@yandex.ru>
2020-12-20 17:54:58 +0300

    fix(transport): do not set default deadline

--- transport/connection.go
+++ transport/connection.go
@@ -17,34 +17,44 @@ type Connection struct {
 }
 
 // Send sends message from buffer using MTProto connection.
-func (c Connection) Send(ctx context.Context, b *bin.Buffer) error {
-	if err := c.conn.SetWriteDeadline(deadline(ctx)); err != nil {
-		return xerrors.Errorf("set write deadline: %w", err)
+func (c Connection) Send(ctx context.Context, b *bin.Buffer) error { // nolint:dupl
+	deadline, ok := ctx.Deadline()
+	if ok {
+		if err := c.conn.SetWriteDeadline(deadline); err != nil {
+			return xerrors.Errorf("set write deadline: %w", err)
+		}
 	}
 
 	if err := c.codec.Write(c.conn, b); err != nil {
 		return xerrors.Errorf("send: %w", err)
 	}
 
-	if err := c.conn.SetWriteDeadline(time.Time{}); err != nil {
-		return xerrors.Errorf("reset write deadline: %w", err)
+	if ok {
+		if err := c.conn.SetWriteDeadline(time.Time{}); err != nil {
+			return xerrors.Errorf("reset write deadline: %w", err)
+		}
 	}
 
 	return nil
 }
 
 // Recv reads message to buffer using MTProto connection.
-func (c Connection) Recv(ctx context.Context, b *bin.Buffer) error {
-	if err := c.conn.SetReadDeadline(deadline(ctx)); err != nil {
-		return xerrors.Errorf("set read deadline: %w", err)
+func (c Connection) Recv(ctx context.Context, b *bin.Buffer) error { // nolint:dupl
+	deadline, ok := ctx.Deadline()
+	if ok {
+		if err := c.conn.SetReadDeadline(deadline); err != nil {
+			return xerrors.Errorf("set read deadline: %w", err)
+		}
 	}
 
 	if err := c.codec.Read(c.conn, b); err != nil {
 		return xerrors.Errorf("recv: %w", err)
 	}
 
-	if err := c.conn.SetReadDeadline(time.Time{}); err != nil {
-		return xerrors.Errorf("reset read deadline: %w", err)
+	if ok {
+		if err := c.conn.SetReadDeadline(time.Time{}); err != nil {
+			return xerrors.Errorf("reset read deadline: %w", err)
+		}
 	}
 
 	return nil
