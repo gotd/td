d2d0ef0fb236105650d7307a6452e207d54a2a79
Aleksandr Razumov <ar@gortc.io>
2020-12-23 21:39:17 +0300

    ci: simplify coverage report

--- go.coverage.sh
+++ go.coverage.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-touch coverage.out
 
-echo 'mode: atomic' > coverage.out
-go list ./... | grep -v /cmd | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -covermode=atomic -coverprofile=profile.out -coverpkg $(go list ./... ) {} && tail -n +2 profile.out >> coverage.out || exit 255' && rm coverage.tmp
+go test -v -coverpkg=./... -coverprofile=profile.out ./...
+go tool cover -func profile.out
