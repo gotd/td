375d7c232f91cf304bb8fa51fa6eb6ddc57046f3
Aleksandr Razumov <ar@gortc.io>
2020-12-24 12:04:00 +0300

    test(telegram): add TestClient_Self

---
+++ telegram/self_test.go
@@ -0,0 +1,29 @@
+package telegram
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gotd/td/bin"
+	"github.com/gotd/td/tg"
+)
+
+func TestClient_Self(t *testing.T) {
+	ctx := context.Background()
+	self, err := newTestClient(func(id int64, body bin.Encoder) (bin.Encoder, error) {
+		assert.IsType(t, &tg.UsersGetUsersRequest{}, body)
+		u := &tg.User{ID: 1}
+		u.SetBot(true)
+		return &tg.UserClassVector{
+			Elems: []tg.UserClass{u},
+		}, nil
+	}).Self(ctx)
+	require.NoError(t, err)
+
+	expected := &tg.User{ID: 1}
+	expected.SetBot(true)
+	require.Equal(t, expected, self)
+}
