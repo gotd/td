42a5f10234b97377d36e9f589be63c60487d7864
Aleksandr Razumov <ar@gortc.io>
2020-12-24 23:46:17 +0300

    test(telegram): improve TestClient_AuthBot

--- telegram/auth_bot_test.go
+++ telegram/auth_bot_test.go
@@ -13,14 +13,21 @@ import (
 
 func TestClient_AuthBot(t *testing.T) {
 	const token = "12345:token"
-	require.NoError(t, newTestClient(func(id int64, body bin.Encoder) (bin.Encoder, error) {
-		assert.Equal(t, &tg.AuthImportBotAuthorizationRequest{
-			BotAuthToken: token,
-			APIID:        TestAppID,
-			APIHash:      TestAppHash,
-		}, body)
-		u := &tg.User{}
-		u.SetBot(true)
-		return &tg.AuthAuthorization{User: u}, nil
-	}).AuthBot(context.Background(), token))
+	t.Run("AuthAuthorization", func(t *testing.T) {
+		require.NoError(t, newTestClient(func(id int64, body bin.Encoder) (bin.Encoder, error) {
+			assert.Equal(t, &tg.AuthImportBotAuthorizationRequest{
+				BotAuthToken: token,
+				APIID:        TestAppID,
+				APIHash:      TestAppHash,
+			}, body)
+			u := &tg.User{}
+			u.SetBot(true)
+			return &tg.AuthAuthorization{User: u}, nil
+		}).AuthBot(context.Background(), token))
+	})
+	t.Run("AuthAuthorizationSignUpRequired", func(t *testing.T) {
+		require.Error(t, newTestClient(func(id int64, body bin.Encoder) (bin.Encoder, error) {
+			return &tg.AuthAuthorizationSignUpRequired{}, nil
+		}).AuthBot(context.Background(), token))
+	})
 }
