920cadabdc0435cee2c16682d93d86d9b9a7825c
tdakkota <tanc13@yandex.ru>
2020-12-25 21:01:20 +0300

    fix(transport): check given MTProxy secret

--- transport/mtproxy.go
+++ transport/mtproxy.go
@@ -1,16 +1,24 @@
 package transport
 
-import "github.com/gotd/td/internal/proto/codec"
+import (
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/internal/proto/codec"
+)
 
 // MTProxy creates MTProxy obfuscated transport.
 //
 // See https://core.telegram.org/mtproto/mtproto-transports#transport-obfuscation
-func MTProxy(d Dialer, dc int, secret []byte) *Transport {
+func MTProxy(d Dialer, dc int, secret []byte) (*Transport, error) {
+	if len(secret) != 16 {
+		return nil, xerrors.Errorf("invalid secret: secret must have length of 16 bytes, got %d", len(secret))
+	}
+
 	return NewTransport(d, func() Codec {
 		return &codec.MTProxyObfuscated2{
 			Codec:  codec.PaddedIntermediate{},
 			DC:     dc,
 			Secret: secret,
 		}
-	})
+	}), nil
 }
