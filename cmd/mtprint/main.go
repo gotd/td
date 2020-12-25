dd49c0ed6ca6ff58c466cae0aff196fae6ec2f7f
Aleksandr Razumov <ar@gortc.io>
2020-12-25 14:22:57 +0300

    feat(mtprint): add mtproto binary file printer

---
+++ cmd/mtprint/main.go
@@ -0,0 +1,48 @@
+// Binary mtprint pretty-prints MTProto message from binary file.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gotd/td/bin"
+	"github.com/gotd/td/internal/mt"
+	"github.com/gotd/td/internal/tmap"
+	"github.com/gotd/td/tg"
+)
+
+func main() {
+	// TODO: Streaming mode.
+	flag.Parse()
+	name := flag.Arg(0)
+	if name == "" {
+		panic("no file provided")
+	}
+
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+
+	b := &bin.Buffer{Buf: buf}
+	id, err := b.PeekID()
+	if err != nil {
+		panic(err)
+	}
+
+	c := tmap.NewConstructor(
+		tg.TypesConstructorMap(),
+		mt.TypesConstructorMap(),
+	)
+	v := c.New(id)
+	if v == nil {
+		panic(fmt.Sprintf("failed to find type 0x%x", id))
+	}
+
+	if err := v.Decode(b); err != nil {
+		panic(err)
+	}
+
+	fmt.Print(v)
+}
