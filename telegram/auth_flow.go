c2f6a5c5c9165f3678837d8d15712f8902a49f42
tdakkota <tanc13@yandex.ru>
2020-12-26 00:00:17 +0300

    fix(telegram): use lower number to generate test phone number

--- telegram/auth_flow.go
+++ telegram/auth_flow.go
@@ -145,7 +145,7 @@ func CodeOnlyAuth(phone string, code CodeAuthenticator) UserAuthenticator {
 func TestAuth(randReader io.Reader, dc int) UserAuthenticator {
 	// 99966XYYYY, X = dc_id, Y = random numbers, code = X repeat 5.
 	// The n value is from 0000 to 9999.
-	n, err := crypto.RandInt64n(randReader, 10000)
+	n, err := crypto.RandInt64n(randReader, 1000)
 	if err != nil {
 		panic(err)
 	}
