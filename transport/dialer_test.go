78c8b8f732596e31e1ae6f7290de89e254e98e70
yhyddr <abserari@gmail.com>
2021-01-05 20:10:13 +0800

    test(transport): add example for socks5 proxy

--- transport/dialer_test.go
+++ transport/dialer_test.go
@@ -3,6 +3,7 @@ package transport_test
 import (
 	"context"
 	"fmt"
+	"net"
 	"time"
 
 	"golang.org/x/net/proxy"
@@ -19,6 +20,31 @@ func ExampleDialFunc() {
 	defer cancel()
 	client := telegram.NewClient(1, "appHash", telegram.Options{
 		Transport: trp,
+	})
+
+	_ = client.Run(ctx, func(ctx context.Context) error {
+		fmt.Println("Started")
+		return nil
+	})
+}
+
+// ExampleDialer for socks5. Methods form https://stackoverflow.com/questions/59456936/socks5-proxy-client-with-context-support
+func ExampleDialer() {
+	// No error would be return.
+	sock5, _ := proxy.SOCKS5("tcp", "IP:PORT", &proxy.Auth{
+		User:     "YOURUSERNAME",
+		Password: "YOURPASSWORD",
+	}, proxy.Direct)
+
+	dc := sock5.(interface {
+		DialContext(ctx context.Context, network, addr string) (net.Conn, error)
+	})
+
+	// Creating connection.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	client := telegram.NewClient(1, "appHash", telegram.Options{
+		Transport: transport.Intermediate(transport.DialFunc(dc.DialContext)),
 	})
 
 	_ = client.Run(ctx, func(ctx context.Context) error {
