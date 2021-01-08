155719f558179a9a2dd3f151ae3065b2a6df031a
Aleksandr Razumov <ar@gortc.io>
2021-01-08 11:58:33 +0300

    refactor(mtproto): move Session to separate file

--- mtproto/conn.go
+++ mtproto/conn.go
@@ -31,38 +31,6 @@ type MessageIDSource interface {
 	New(t proto.MessageType) int64
 }
 
-// Session represents connection state.
-type Session struct {
-	ID   int64
-	Key  crypto.AuthKey
-	Salt int64
-}
-
-// Session returns current connection session info.
-func (c *Conn) session() Session {
-	c.sessionMux.RLock()
-	defer c.sessionMux.RUnlock()
-	return Session{
-		Key:  c.authKey,
-		Salt: c.salt,
-		ID:   c.sessionID,
-	}
-}
-
-// newSessionID sets session id to random value.
-func (c *Conn) newSessionID() error {
-	id, err := crypto.RandInt64(c.rand)
-	if err != nil {
-		return err
-	}
-
-	c.sessionMux.Lock()
-	defer c.sessionMux.Unlock()
-	c.sessionID = id
-
-	return nil
-}
-
 // Cipher handles message encryption and decryption.
 type Cipher interface {
 	DecryptFromBuffer(k crypto.AuthKey, buf *bin.Buffer) (*crypto.EncryptedMessageData, error)
