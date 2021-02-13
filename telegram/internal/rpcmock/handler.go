13c18ddfb059cb42fcb81c07e8bcfcdc4608cf5b
tdakkota <tanc13@yandex.ru>
2021-02-13 18:01:42 +0300

    feat: add some testing utils

---
+++ telegram/internal/rpcmock/handler.go
@@ -0,0 +1,16 @@
+package rpcmock
+
+import "github.com/gotd/td/bin"
+
+// Handler is a RPC call handler.
+type Handler interface {
+	Handle(id int64, body bin.Encoder) (bin.Encoder, error)
+}
+
+// HandlerFunc is a function adapter for Handler.
+type HandlerFunc func(id int64, body bin.Encoder) (bin.Encoder, error)
+
+// Handle implements Handler.
+func (h HandlerFunc) Handle(id int64, body bin.Encoder) (bin.Encoder, error) {
+	return h(id, body)
+}
---
+++ telegram/internal/rpcmock/rpcmock.go
@@ -0,0 +1,67 @@
+package rpcmock
+
+import (
+	"github.com/stretchr/testify/require"
+
+	"github.com/gotd/td/bin"
+)
+
+// TestingT is simplified *testing.T interface.
+type TestingT interface {
+	Cleanup(cb func())
+}
+
+// Mock is a mock tg.Invoker.
+type Mock struct {
+	calls []Handler
+	*require.Assertions
+}
+
+// NewMock creates new Mock.
+func NewMock(testingT TestingT, assert *require.Assertions) *Mock {
+	m := &Mock{
+		Assertions: assert,
+	}
+
+	testingT.Cleanup(func() {
+		m.Assertions.Truef(
+			m.AllWereMet(),
+			"not all expected calls happen (expected yet %d)",
+			len(m.calls),
+		)
+	})
+	return m
+}
+
+// AllWereMet returns true if all expected calls happened.
+func (i *Mock) AllWereMet() bool {
+	return len(i.calls) == 0
+}
+
+func (i *Mock) add(h Handler) *Mock {
+	i.calls = append(i.calls, h)
+	return i
+}
+
+func (i *Mock) pop() (Handler, bool) {
+	if len(i.calls) < 1 {
+		return nil, false
+	}
+
+	h := i.calls[0]
+	// Delete from SliceTricks.
+	copy(i.calls, i.calls[1:])
+	i.calls[len(i.calls)-1] = nil
+	i.calls = i.calls[:len(i.calls)-1]
+	return h, true
+}
+
+// Handler returns HandlerFunc of Mock.
+func (i *Mock) Handler() HandlerFunc {
+	return func(id int64, body bin.Encoder) (bin.Encoder, error) {
+		h, ok := i.pop()
+		i.Assertions.Truef(ok, "unexpected call")
+
+		return h.Handle(id, body)
+	}
+}
