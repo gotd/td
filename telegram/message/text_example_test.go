57953395c7d06e2f278a3388b44e9fbbb030fdaf
tdakkota <tanc13@yandex.ru>
2021-03-01 11:13:33 +0300

    docs(message): add Text and StyledText examples

---
+++ telegram/message/text_example_test.go
@@ -0,0 +1,35 @@
+package message_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
+)
+
+func sendText(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	// This example creates a plaing message and sends it
+	// to your Saved Message folder.
+	return client.Run(ctx, func(ctx context.Context) error {
+		return message.NewSender(tg.NewClient(client)).Self().Text(ctx, "Hi!")
+	})
+}
+
+func ExampleBuilder_Text() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := sendText(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
