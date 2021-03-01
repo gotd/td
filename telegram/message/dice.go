b45fa77f2043c37e6abdd8d5f18d85d3d36b0206
tdakkota <tanc13@yandex.ru>
2021-03-01 11:00:25 +0300

    feat(message): add dice helpers, add dice example

--- telegram/message/dice.go
+++ telegram/message/dice.go
@@ -1,6 +1,10 @@
 package message
 
-import "github.com/gotd/td/tg"
+import (
+	"context"
+
+	"github.com/gotd/td/tg"
+)
 
 // MediaDice adds a dice-based animated sticker.
 func MediaDice(emoticon string) MediaOption {
@@ -22,4 +26,19 @@ func Darts() MediaOption {
 // Basketball adds a basketball animated sticker.
 func Basketball() MediaOption {
 	return MediaDice("🏀")
+}
+
+// Dice sends a dice animated sticker.
+func (b *Builder) Dice(ctx context.Context) error {
+	return b.Media(ctx, Dice())
+}
+
+// Darts sends a darts animated sticker.
+func (b *Builder) Darts(ctx context.Context) error {
+	return b.Media(ctx, Darts())
+}
+
+// Basketball sends a basketball animated sticker.
+func (b *Builder) Basketball(ctx context.Context) error {
+	return b.Media(ctx, Basketball())
 }
---
+++ telegram/message/dice_example_test.go
@@ -0,0 +1,45 @@
+package message_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
+)
+
+func sendDice(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		sender := message.NewSender(tg.NewClient(client))
+
+		// Sends dice "🎲" to the @durovschat.
+		if err := sender.Resolve("@durovschat").Dice(ctx); err != nil {
+			return err
+		}
+
+		// Sends darts "🎯" to the @durovschat.
+		if err := sender.Resolve("https://t.me/durovschat").Darts(ctx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func ExampleMediaDice() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := sendDice(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
