091eeb2763611081811eb394b01c00cb95ac0c30
Aleksandr Razumov <ar@gortc.io>
2021-03-02 13:23:28 +0300

    feat(tdp,gen): implement basic pretty print

---
+++ tdp/internal/generate.go
@@ -0,0 +1,3 @@
+package internal
+
+//go:generate go run github.com/gotd/td/cmd/gotdgen --format=true --clean --package schema --target schema --schema _testdata/schema.tl
---
+++ tdp/type.go
@@ -0,0 +1,25 @@
+package tdp
+
+// Field of TL type, non-recursive.
+type Field struct {
+	Name       string
+	SchemaName string
+	Null       bool
+}
+
+// Type info for TL type, non-recursive.
+type Type struct {
+	// Name in TL schema.
+	Name string
+	// ID is type id.
+	ID int
+	// Fields of type.
+	Fields []Field
+	// Null denotes whether value is null.
+	Null bool
+}
+
+// Object of TL schema that can return type info.
+type Object interface {
+	TypeInfo() Type
+}
