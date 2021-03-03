9d69c2fd92d0d4abd67a2f335f950a78ca512833
tdakkota <tanc13@yandex.ru>
2021-03-03 15:45:39 +0300

    fix(message): resolve domain once and store using atomic

--- telegram/message/peer.go
+++ telegram/message/peer.go
@@ -3,24 +3,50 @@ package message
 import (
 	"context"
 
+	"go.uber.org/atomic"
 	"golang.org/x/xerrors"
 
+	"github.com/gotd/td/telegram/message/peer"
 	"github.com/gotd/td/tg"
 )
 
-func (s *Sender) builder(peer peerPromise) *RequestBuilder {
+type resolvedCache atomic.Value
+
+func (r *resolvedCache) Store(result tg.InputPeerClass) {
+	r.Value.Store(result)
+}
+
+func (r *resolvedCache) Load() (result tg.InputPeerClass, ok bool) {
+	result, ok = r.Value.Load().(tg.InputPeerClass)
+	return
+}
+
+func (s *Sender) builder(promise peerPromise) *RequestBuilder {
+	once := resolvedCache{}
+
 	return &RequestBuilder{
 		Builder: Builder{
 			sender: s,
-			peer:   peer,
+			peer: func(ctx context.Context) (r tg.InputPeerClass, err error) {
+				if v, ok := once.Load(); ok {
+					return v, nil
+				}
+				defer func() {
+					if err == nil && r != nil {
+						once.Store(r)
+					}
+				}()
+
+				return promise(ctx)
+			},
 		},
 	}
 }
 
 // Peer uses given peer to create new message builder.
-func (s *Sender) Peer(peer tg.InputPeerClass) *RequestBuilder {
+func (s *Sender) Peer(p tg.InputPeerClass) *RequestBuilder {
 	return s.builder(func(ctx context.Context) (tg.InputPeerClass, error) {
-		return peer, nil
+		return p, nil
 	})
 }
 
@@ -36,52 +62,10 @@ type AnswerableMessageUpdate interface {
 	GetPts() int
 }
 
-type entities struct {
-	Users    map[int]*tg.User
-	Chats    map[int]*tg.Chat
-	Channels map[int]*tg.Channel
-}
-
-func findPeer(uctx entities, peerID tg.PeerClass) (tg.InputPeerClass, error) {
-	var peer tg.InputPeerClass
-	switch p := peerID.(type) {
-	case *tg.PeerUser: // peerUser#9db1bc6d
-		dialog, ok := uctx.Users[p.UserID]
-		if !ok {
-			return nil, xerrors.Errorf("user %d not found in Update", p.UserID)
-		}
-
-		peer = &tg.InputPeerUser{
-			UserID:     dialog.ID,
-			AccessHash: dialog.AccessHash,
-		}
-	case *tg.PeerChat: // peerChat#bad0e5bb
-		dialog, ok := uctx.Chats[p.ChatID]
-		if !ok {
-			return nil, xerrors.Errorf("chat %d not found in Update", p.ChatID)
-		}
-
-		peer = &tg.InputPeerChat{
-			ChatID: dialog.ID,
-		}
-	case *tg.PeerChannel: // peerChannel#bddde532
-		dialog, ok := uctx.Channels[p.ChannelID]
-		if !ok {
-			return nil, xerrors.Errorf("channel %d not found in Update", p.ChannelID)
-		}
-
-		peer = &tg.InputPeerChannel{
-			ChannelID:  dialog.ID,
-			AccessHash: dialog.AccessHash,
-		}
-	}
-
-	return peer, nil
-}
-
 // Answer uses given message update to create message for same chat.
 func (s *Sender) Answer(uctx tg.UpdateContext, upd AnswerableMessageUpdate) *RequestBuilder {
-	return s.builder(func(ctx context.Context) (tg.InputPeerClass, error) {
+	entities := peer.EntitiesFromUpdate(uctx)
+	return s.builder(func(ctx context.Context) (r tg.InputPeerClass, err error) {
 		updMsg := upd.GetMessage()
 		msg, ok := updMsg.AsNotEmpty()
 		if !ok {
@@ -90,23 +74,15 @@ func (s *Sender) Answer(uctx tg.UpdateContext, upd AnswerableMessageUpdate) *Req
 				return nil, xerrors.Errorf("unexpected type %T", updMsg)
 			}
 
-			peer, ok := emptyMsg.GetPeerID()
+			p, ok := emptyMsg.GetPeerID()
 			if !ok {
 				return nil, xerrors.Errorf("got %T with empty PeerID", updMsg)
 			}
 
-			return findPeer(entities{
-				Users:    uctx.Users,
-				Chats:    uctx.Chats,
-				Channels: uctx.Channels,
-			}, peer)
+			return entities.ExtractPeer(p)
 		}
 
-		return findPeer(entities{
-			Users:    uctx.Users,
-			Chats:    uctx.Chats,
-			Channels: uctx.Channels,
-		}, msg.GetPeerID())
+		return entities.ExtractPeer(msg.GetPeerID())
 	})
 }
 
