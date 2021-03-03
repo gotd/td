57a45a844eec3f42f911e56b379721b626fefd81
tdakkota <tanc13@yandex.ru>
2021-03-03 15:49:15 +0300

    docs: add gif send example

---
+++ telegram/message/gif_example_test.go
@@ -0,0 +1,42 @@
+package message_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
+)
+
+func sendGif(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		sender := message.NewSender(tg.NewClient(client))
+
+		// Uploads and sends gif to the @durovschat.
+		if err := sender.Resolve("https://t.me/durovschat").
+			Upload(message.FromPath("./rickroll.gif")).
+			GIF(ctx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func ExampleGIF() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := sendGif(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
