274e0110abc39a25807b14ca560ea68d13d5e685
tdakkota <tanc13@yandex.ru>
2021-03-03 18:50:09 +0300

    docs(message): add some sending examples

---
+++ telegram/message/audio_example_test.go
@@ -0,0 +1,61 @@
+package message_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/telegram/uploader"
+	"github.com/gotd/td/tg"
+)
+
+func sendAudio(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		raw := tg.NewClient(client)
+		f, err := uploader.NewUploader(raw).FromPath(ctx, "vsyo idyot po planu.mp3")
+		if err != nil {
+			return xerrors.Errorf("upload: %w", err)
+		}
+
+		sender := message.NewSender(raw)
+		r := sender.Resolve("@durovschat")
+
+		// Uploads and sends audio to the @durovschat.
+		if err := r.Audio(ctx, f); err != nil {
+			return err
+		}
+
+		// Uploads and sends audio with title to the @durovschat.
+		if err := r.Media(ctx, message.Audio(f).
+			Title("Yegor Letov — Everything is going according to plan")); err != nil {
+			return err
+		}
+
+		// Uploads and sends voice message to the @durovschat.
+		if err := r.Voice(ctx, f); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func ExampleAudio() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := sendAudio(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
---
+++ telegram/message/draft_example_test.go
@@ -0,0 +1,53 @@
+package message_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
+)
+
+func saveDraft(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		sender := message.NewSender(tg.NewClient(client))
+		r := sender.Resolve("@durov")
+
+		// Save draft message.
+		if err := r.SaveDraft(ctx, "Hi!"); err != nil {
+			return xerrors.Errorf("draft: %w", err)
+		}
+
+		// Save styled draft message.
+		if err := r.SaveStyledDraft(ctx, message.Bold("Hi!")); err != nil {
+			return xerrors.Errorf("draft: %w", err)
+		}
+
+		// Clear draft for resolved @durov peer.
+		if err := r.ClearDraft(ctx); err != nil {
+			return xerrors.Errorf("draft: %w", err)
+		}
+
+		return nil
+	})
+}
+
+func ExampleBuilder_SaveDraft() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := saveDraft(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
