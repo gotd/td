94f29d4b7c97fd2a2c81feeaa87b0f33e567ec5b
tdakkota <tanc13@yandex.ru>
2021-03-12 19:14:59 +0300

    refactor(message): upload is now method of Builder, not RequestBuilder

--- telegram/message/upload.go
+++ telegram/message/upload.go
@@ -2,92 +2,25 @@ package message
 
 import (
 	"context"
-	"time"
 
 	"golang.org/x/xerrors"
 
-	"github.com/gotd/td/telegram/message/markup"
 	"github.com/gotd/td/tg"
 )
 
 // Upload creates new UploadBuilder to upload and send attachments.
 // Given option will be called only once, even if you call upload functions .
-func (b *RequestBuilder) Upload(upd UploadOption) *UploadBuilder {
+func (b *Builder) Upload(upd UploadOption) *UploadBuilder {
 	return &UploadBuilder{
-		builder: *b,
+		builder: b.copy(),
 		option:  upd,
 	}
 }
 
 // UploadBuilder is a attachment uploading helper.
 type UploadBuilder struct {
-	builder RequestBuilder
+	builder *Builder
 	option  UploadOption
-}
-
-func (u *UploadBuilder) next() *UploadBuilder {
-	return u
-}
-
-// Silent sets flag to send this message silently (no notifications for the receivers).
-func (u *UploadBuilder) Silent() *UploadBuilder {
-	r := u.next()
-	r.builder.silent = true
-	return r
-}
-
-// Background sets flag to send this message as background message.
-func (u *UploadBuilder) Background() *UploadBuilder {
-	r := u.next()
-	r.builder.background = true
-	return r
-}
-
-// ClearDraft sets flag to clear the draft field.
-func (u *UploadBuilder) ClearDraft() *UploadBuilder {
-	r := u.next()
-	r.builder.clearDraft = true
-	return r
-}
-
-// Reply sets message ID to reply.
-func (u *UploadBuilder) Reply(id int) *UploadBuilder {
-	r := u.next()
-	r.builder.replyToMsgID = id
-	return r
-}
-
-// ReplyMsg sets message to reply.
-func (u *UploadBuilder) ReplyMsg(msg tg.MessageClass) *UploadBuilder {
-	return u.Reply(msg.GetID())
-}
-
-// Schedule sets scheduled message date for scheduled messages.
-func (u *UploadBuilder) Schedule(date time.Time) *UploadBuilder {
-	r := u.next()
-	r.builder.scheduleDate = int(date.Unix())
-	return r
-}
-
-// NoWebpage sets flag to disable generation of the webpage preview.
-func (u *UploadBuilder) NoWebpage() *UploadBuilder {
-	r := u.next()
-	r.builder.noWebpage = true
-	return r
-}
-
-// Markup sets reply markup for sending bot buttons.
-// NB: markup will not be used, if you send multiple media attachments.
-func (u *UploadBuilder) Markup(m tg.ReplyMarkupClass) *UploadBuilder {
-	r := u.next()
-	r.builder.replyMarkup = m
-	return r
-}
-
-// Row sets single row keyboard markup for sending bot buttons.
-// NB: markup will not be used, if you send multiple media attachments.
-func (u *UploadBuilder) Row(button tg.KeyboardButtonClass, buttons ...tg.KeyboardButtonClass) *UploadBuilder {
-	return u.Markup(markup.SingleRow(button, buttons...))
 }
 
 func (u *UploadBuilder) file(ctx context.Context) (tg.InputFileClass, error) {
