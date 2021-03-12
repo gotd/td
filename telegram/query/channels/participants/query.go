cf7e55127448e4cce129d917b33aecf505e10b50
tdakkota <tanc13@yandex.ru>
2021-03-12 15:33:46 +0300

    feat(query): add channel participants iterator

---
+++ telegram/query/channels/participants/query.go
@@ -0,0 +1,4 @@
+package participants
+
+//go:generate go run github.com/gotd/td/telegram/query/internal/gen -result=ChannelsChannelParticipantsClass -package=participants -prefix=Channels -out=queries.gen.go
+
