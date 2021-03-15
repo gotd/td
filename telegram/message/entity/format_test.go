f50fc6270a91eb8ef07f9b858aeb60b43ac5043d
tdakkota <tanc13@yandex.ru>
2021-03-15 15:21:36 +0300

    fix(entity): use UTF-16 to count codepoints

--- telegram/message/entity/format_test.go
+++ telegram/message/entity/format_test.go
@@ -5,148 +5,29 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/gotd/td/tg"
+	"github.com/gotd/td/internal/testutil"
 )
 
-func TestFormat(t *testing.T) {
-	b := Builder{}
-	t.Run("Mention", func(t *testing.T) {
-		_, ent := b.Mention("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityMention{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Hashtag", func(t *testing.T) {
-		_, ent := b.Hashtag("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityHashtag{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("BotCommand", func(t *testing.T) {
-		_, ent := b.BotCommand("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityBotCommand{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("URL", func(t *testing.T) {
-		_, ent := b.URL("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityURL{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Email", func(t *testing.T) {
-		_, ent := b.Email("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityEmail{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Bold", func(t *testing.T) {
-		_, ent := b.Bold("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityBold{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Italic", func(t *testing.T) {
-		_, ent := b.Italic("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityItalic{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Code", func(t *testing.T) {
-		_, ent := b.Code("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityCode{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Pre", func(t *testing.T) {
-		_, ent := b.Pre("abc", "lang").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityPre{
-			Offset:   0,
-			Length:   len("abc"),
-			Language: "lang",
-		}, r)
-	})
-	t.Run("TextURL", func(t *testing.T) {
-		_, ent := b.TextURL("abc", "url").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityTextURL{
-			Offset: 0,
-			Length: len("abc"),
-			URL:    "url",
-		}, r)
-	})
-	t.Run("MentionName", func(t *testing.T) {
-		_, ent := b.MentionName("abc", 1).Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityMentionName{
-			Offset: 0,
-			Length: len("abc"),
-			UserID: 1,
-		}, r)
-	})
-	t.Run("Phone", func(t *testing.T) {
-		_, ent := b.Phone("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityPhone{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Cashtag", func(t *testing.T) {
-		_, ent := b.Cashtag("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityCashtag{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Underline", func(t *testing.T) {
-		_, ent := b.Underline("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityUnderline{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Strike", func(t *testing.T) {
-		_, ent := b.Strike("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityStrike{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("Blockquote", func(t *testing.T) {
-		_, ent := b.Blockquote("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityBlockquote{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
-	t.Run("BankCard", func(t *testing.T) {
-		_, ent := b.BankCard("abc").Complete()
-		r := ent[0]
-		require.Equal(t, &tg.MessageEntityBankCard{
-			Offset: 0,
-			Length: len("abc"),
-		}, r)
-	})
+func Test_computeLength(t *testing.T) {
+	tests := []struct {
+		s    string
+		want int
+	}{
+		{"a🏳️‍🌈", 7},
+		{"a🏳️‍🌈🏳️‍🌈", 13},
+		{"a🏳️‍🌈🏳️‍🌈a🏳️‍🌈🏳️‍🌈", 26},
+		{"a👨‍👦‍👦", 9},
+		{`message#bce383d2
+  id: 1939
+  from_id: 🏳️‍🌈
+  date: 2021-03-15T10:01:41Z`, 74},
+	}
+	for _, tt := range tests {
+		testutil.ZeroAlloc(t, func() {
+			computeLength(tt.s)
+		})
+		t.Run(tt.s, func(t *testing.T) {
+			require.Equal(t, tt.want, computeLength(tt.s))
+		})
+	}
 }
