d0bcb8128e94a60ee08b3ecfe8f627f78434eee2
rs <yuiop1955@mail.ru>
2021-03-17 00:07:41 +0500

    feat(gen): server dispatcher

--- internal/gen/write_source.go
+++ internal/gen/write_source.go
@@ -142,6 +142,11 @@ func (g *Generator) WriteSource(fs FileSystem, pkgName string, t *template.Templ
 	if err := w.WriteStructs(g.structs, g.mappings); err != nil {
 		return xerrors.Errorf("structs: %w", err)
 	}
+	if err := w.Generate("server", "tl_server_gen.go", config{
+		Structs: g.structs,
+	}); err != nil {
+		return err
+	}
 
 	if g.hasUpdateClass() {
 		if err := w.Generate("handlers", "tl_handlers_gen.go", config{
