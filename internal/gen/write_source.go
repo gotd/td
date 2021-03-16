5043aabd928761325c9280dcb8f7427cee8ecee5
tdakkota <tanc13@yandex.ru>
2021-03-16 22:31:42 +0300

    fix(gen): write dump to the file, not stderr

--- internal/gen/write_source.go
+++ internal/gen/write_source.go
@@ -72,7 +72,7 @@ func (w *writer) Generate(templateName, fileName string, cfg config) error {
 		return xerrors.Errorf("failed to execute template %s for %s: %w", templateName, fileName, err)
 	}
 	if err := w.fs.WriteFile(fileName, w.buf.Bytes()); err != nil {
-		_ = os.WriteFile(fileName+".dump", w.buf.Bytes(), 755)
+		_ = os.WriteFile(fileName+".dump", w.buf.Bytes(), 0755)
 		return xerrors.Errorf("failed to write file %s: %w", fileName, err)
 	}
 	w.wrote[fileName] = true
