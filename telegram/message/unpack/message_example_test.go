82765a9f519f45ecc747ba7989ea0bac0a165030
tdakkota <tanc13@yandex.ru>
2021-04-15 18:20:40 +0300

    docs(unpack): add Message example

---
+++ telegram/message/unpack/message_example_test.go
@@ -0,0 +1,44 @@
+package unpack_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/telegram/message/unpack"
+	"github.com/gotd/td/tg"
+)
+
+func unpackMessage(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		sender := message.NewSender(tg.NewClient(client))
+
+		msg, err := unpack.Message(sender.Resolve("@durovschat").Dice(ctx))
+		// Sends dice "🎲" to the @durovschat.
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Sent message ID:", msg.ID)
+
+		return nil
+	})
+}
+
+func ExampleMessage() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := unpackMessage(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
