f658ea0f8363ced08ac91b844e17ea2a108d0a73
tdakkota <tanc13@yandex.ru>
2021-04-15 22:59:03 +0300

    docs(peer): add LRU example

---
+++ telegram/message/peer/lru_example_test.go
@@ -0,0 +1,52 @@
+package peer_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/telegram/message/peer"
+	"github.com/gotd/td/tg"
+)
+
+func resolveLRU(ctx context.Context) error {
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		return err
+	}
+
+	return client.Run(ctx, func(ctx context.Context) error {
+		raw := tg.NewClient(client)
+		resolver := peer.NewLRUResolver(peer.Plain(raw), 16).WithExpiration(time.Minute)
+		sender := message.NewSender(raw).WithResolver(resolver)
+
+		// "durovschat" will be resolved by Plain resolver.
+		if _, err := sender.Resolve("@durovschat").Dice(ctx); err != nil {
+			return err
+		}
+
+		// "durovschat" will be resolved by cache.
+		if _, err := sender.Resolve("https://t.me/durovschat").Darts(ctx); err != nil {
+			return err
+		}
+
+		// Evict and delete record.
+		resolver.Evict("durovschat")
+
+		return nil
+	})
+}
+
+func ExampleLRUResolver_cache() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := resolveLRU(ctx); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+}
