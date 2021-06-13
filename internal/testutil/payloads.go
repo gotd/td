ca226f64be5118fa92f45a7614bb30b27a1eb0ba
tdakkota <tanc13@yandex.ru>
2021-06-13 21:09:38 +0300

    test(codec): add codec benchmarks

--- internal/testutil/payloads.go
+++ internal/testutil/payloads.go
@@ -1,5 +1,10 @@
 package testutil
 
+import (
+	"fmt"
+	"testing"
+)
+
 // Payloads returns a payload sizes list.
 // Helpful for benchmarks.
 func Payloads() []int {
@@ -10,5 +15,12 @@ func Payloads() []int {
 		8192,
 		64 * 1024,
 		512 * 1024,
+	}
+}
+
+// RunPayloads runs given benchmark runner for every payload.
+func RunPayloads(b *testing.B, runner func(size int) func(b *testing.B)) {
+	for _, size := range Payloads() {
+		b.Run(fmt.Sprintf("%db", size), runner(size))
 	}
 }
