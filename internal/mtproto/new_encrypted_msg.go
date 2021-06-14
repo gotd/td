ec8847b5dc03523a66134d4f063108c77341c955
tdakkota <tanc13@yandex.ru>
2021-06-14 18:03:50 +0300

    fix(mtproto): encode lazily if compression is disabled

--- internal/mtproto/new_encrypted_msg.go
+++ internal/mtproto/new_encrypted_msg.go
@@ -10,36 +10,46 @@ import (
 )
 
 func (c *Conn) newEncryptedMessage(id int64, seq int32, payload bin.Encoder, b *bin.Buffer) error {
-	payloadBuf := bufPool.Get()
-	defer bufPool.Put(payloadBuf)
-	if err := payload.Encode(payloadBuf); err != nil {
-		return xerrors.Errorf("encode payload: %w", err)
-	}
-
 	c.logWithType(b).Debug("Request", zap.Int64("msg_id", id))
 	s := c.session()
 
-	var d crypto.EncryptedMessageData
 	// TODO(tdakkota): Smarter gzip.
 	// 	1) Generate Length() method for every encoder, to count length without encoding.
 	// 	2) Re-use buffer instead of using yet one.
 	// 	3) Do not send proto.GZIP if gzipped size is equal or bigger.
-	if c.compressThreshold > 0 && payloadBuf.Len() > c.compressThreshold {
+	var d crypto.EncryptedMessageData
+	if c.compressThreshold <= 0 {
 		d = crypto.EncryptedMessageData{
 			SessionID: s.ID,
 			Salt:      s.Salt,
 			MessageID: id,
 			SeqNo:     seq,
-			Message:   proto.GZIP{Data: payloadBuf.Raw()},
+			Message:   payload,
 		}
 	} else {
-		d = crypto.EncryptedMessageData{
-			SessionID:              s.ID,
-			Salt:                   s.Salt,
-			MessageID:              id,
-			SeqNo:                  seq,
-			MessageDataLen:         int32(payloadBuf.Len()),
-			MessageDataWithPadding: payloadBuf.Buf,
+		payloadBuf := bufPool.Get()
+		defer bufPool.Put(payloadBuf)
+		if err := payload.Encode(payloadBuf); err != nil {
+			return xerrors.Errorf("encode payload: %w", err)
+		}
+
+		if payloadBuf.Len() > c.compressThreshold {
+			d = crypto.EncryptedMessageData{
+				SessionID: s.ID,
+				Salt:      s.Salt,
+				MessageID: id,
+				SeqNo:     seq,
+				Message:   proto.GZIP{Data: payloadBuf.Raw()},
+			}
+		} else {
+			d = crypto.EncryptedMessageData{
+				SessionID:              s.ID,
+				Salt:                   s.Salt,
+				MessageID:              id,
+				SeqNo:                  seq,
+				MessageDataLen:         int32(payloadBuf.Len()),
+				MessageDataWithPadding: payloadBuf.Buf,
+			}
 		}
 	}
 
