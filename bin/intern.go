3547dac23500600c35719b6a0da598f86eb4e9c6
tdakkota <tanc13@yandex.ru>
2021-07-26 14:25:01 +0300

    fix(bin): do not intern one byte strings

--- bin/intern.go
+++ bin/intern.go
@@ -5,5 +5,9 @@ package bin
 import "github.com/josharian/intern"
 
 func strFromBytes(b []byte) string {
+	// One byte string does not cause allocation.
+	if len(b) == 1 {
+		return string(b)
+	}
 	return intern.Bytes(b)
 }
