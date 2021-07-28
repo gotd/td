596d1b98f1f72f2da4e9d5b8a96a58cc145d0965
tdakkota <tanc13@yandex.ru>
2021-07-28 14:48:37 +0300

    fix(mtproto): parse keys in init

--- internal/mtproto/vendored_keys.go
+++ internal/mtproto/vendored_keys.go
@@ -19,11 +19,7 @@ var (
 	}
 )
 
-// vendoredKeys parses vendored file _data/public_keys.pem as list of
-// PEM-encoded public RSA keys.
-//
-// Most recent key list can be found on https://my.telegram.org/apps.
-func vendoredKeys() []*rsa.PublicKey {
+func init() {
 	parsedKeys.Once.Do(func() {
 		keys, err := crypto.ParseRSAPublicKeys(publicKeys)
 		if err != nil {
@@ -31,5 +27,12 @@ func vendoredKeys() []*rsa.PublicKey {
 		}
 		parsedKeys.Keys = keys
 	})
+}
+
+// vendoredKeys parses vendored file _data/public_keys.pem as list of
+// PEM-encoded public RSA keys.
+//
+// Most recent key list can be found on https://my.telegram.org/apps.
+func vendoredKeys() []*rsa.PublicKey {
 	return parsedKeys.Keys
 }
