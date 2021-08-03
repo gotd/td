e7dffc27ff8f63294e04a256ca20dcfd623eb583
tdakkota <tanc13@yandex.ru>
2021-08-03 12:13:50 +0300

    feat(transport): detect codec automatically in Server

---
+++ transport/detect_codec.go
@@ -0,0 +1,35 @@
+package transport
+
+import (
+	"bytes"
+	"io"
+
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/internal/proto/codec"
+)
+
+func detectCodec(c io.Reader) (Codec, io.Reader, error) {
+	var buf [4]byte
+	if _, err := io.ReadFull(c, buf[:1]); err != nil {
+		return nil, nil, xerrors.Errorf("read first byte: %w", err)
+	}
+
+	if buf[0] == codec.AbridgedClientStart[0] {
+		return Abridged.Codec(), c, nil
+	}
+
+	if _, err := io.ReadFull(c, buf[1:4]); err != nil {
+		return nil, nil, xerrors.Errorf("read header: %w", err)
+	}
+	switch buf {
+	case codec.IntermediateClientStart:
+		return Intermediate.Codec(), c, nil
+	case codec.PaddedIntermediateClientStart:
+		return PaddedIntermediate.Codec(), c, nil
+	default:
+		buffered := bytes.NewReader(buf[:])
+		r := io.MultiReader(buffered, c)
+		return Full.Codec(), r, nil
+	}
+}
---
+++ transport/detect_codec_test.go
@@ -0,0 +1,70 @@
+package transport
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gotd/td/internal/proto/codec"
+)
+
+func Test_detectCodec(t *testing.T) {
+	tests := []struct {
+		name       string
+		data       []byte
+		resultType Codec
+		wantErr    bool
+	}{
+		{
+			"Abridged",
+			codec.AbridgedClientStart[:],
+			codec.Abridged{},
+			false,
+		},
+		{
+			"Intermediate",
+			codec.IntermediateClientStart[:],
+			codec.Intermediate{}, false,
+		},
+		{
+			"PaddedIntermediate",
+			codec.PaddedIntermediateClientStart[:],
+			codec.PaddedIntermediate{},
+			false,
+		},
+		{
+			"Full",
+			[]byte{'g', 'o', 't', 'd'},
+			&codec.Full{},
+			false,
+		},
+		{
+			"EOF-first",
+			nil,
+			nil,
+			true,
+		},
+		{
+			"EOF-second",
+			[]byte{'a'},
+			nil,
+			true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			a := require.New(t)
+
+			r, _, err := detectCodec(bytes.NewReader(test.data))
+			if test.wantErr {
+				a.Nil(r)
+				a.Error(err)
+			} else {
+				a.NotNil(r)
+				a.NoError(err)
+				a.IsType(test.resultType, r)
+			}
+		})
+	}
+}
