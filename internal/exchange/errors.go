d8da2c684bfe10aca55cddc51db7743f03cca9d9
tdakkota <tanc13@yandex.ru>
2021-08-20 12:20:06 +0300

    docs(exchange): add ErrKeyFingerprintNotFound description

--- internal/exchange/errors.go
+++ internal/exchange/errors.go
@@ -4,4 +4,6 @@ import (
 	"errors"
 )
 
+// ErrKeyFingerprintNotFound is returned when client can't find keys by fingerprints
+// provided by server during key exchange.
 var ErrKeyFingerprintNotFound = errors.New("key fingerprint not found")
