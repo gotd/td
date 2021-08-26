a7dc8a34995788aab8d0f9933f91d8aba9c16588
tdakkota <tanc13@yandex.ru>
2021-08-26 12:05:48 +0300

    feat(session): add Telegram Desktop session convertor

---
+++ session/tdesktop.go
@@ -0,0 +1,51 @@
+package session
+
+import (
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/session/tdesktop"
+	"github.com/gotd/td/telegram/dcs"
+	"github.com/gotd/td/tg"
+)
+
+func findDCAddr(list []tg.DCOption, dcID int) string {
+	for _, opt := range list {
+		if opt.ID != dcID {
+			continue
+		}
+		if opt.TCPObfuscatedOnly ||
+			opt.CDN ||
+			opt.MediaOnly {
+			continue
+		}
+
+		return opt.IPAddress
+	}
+
+	return ""
+}
+
+// TDesktopSession converts TDesktop's Account to Data.
+func TDesktopSession(a tdesktop.Account) (*Data, error) {
+	auth := a.Authorization
+	dc := auth.MainDC
+
+	key, ok := auth.Keys[dc]
+	if !ok {
+		return nil, xerrors.Errorf("key for main DC (%d) not found", dc)
+	}
+	keyID := key.ID()
+
+	// TODO(tdakkota): distinguish test and production accounts.
+	addr := findDCAddr(dcs.Prod().Options, dc)
+	if addr == "" {
+		return nil, xerrors.Errorf("can't find address for DC %d", dc)
+	}
+
+	return &Data{
+		DC:        dc,
+		Addr:      addr,
+		AuthKey:   key[:],
+		AuthKeyID: keyID[:],
+	}, nil
+}
---
+++ session/tdesktop_example_test.go
@@ -0,0 +1,73 @@
+package session_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gotd/td/session"
+	"github.com/gotd/td/session/tdesktop"
+	"github.com/gotd/td/telegram"
+)
+
+func ExampleTDesktopSession() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	root := filepath.Join(home, "Downloads", "Telegram", "tdata")
+	accounts, err := tdesktop.Read(root, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := session.TDesktopSession(accounts[0])
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data.DC, data.Addr)
+}
+
+func ExampleTDesktopSession_convert() {
+	ctx := context.Background()
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	root := filepath.Join(home, "Downloads", "Telegram", "tdata")
+	accounts, err := tdesktop.Read(root, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := session.TDesktopSession(accounts[0])
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		storage = new(session.StorageMemory)
+		loader  = session.Loader{Storage: storage}
+	)
+
+	// Save decoded Telegram Desktop session as gotd session.
+	if err := loader.Save(ctx, data); err != nil {
+		panic(err)
+	}
+
+	// Create client.
+	client := telegram.NewClient(telegram.TestAppID, telegram.TestAppHash, telegram.Options{
+		SessionStorage: storage,
+	})
+	if err := client.Run(ctx, func(ctx context.Context) error {
+		// Use Telegram Desktop session.
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+}
