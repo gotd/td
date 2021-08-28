d5af297e03744be0eb24cff959dd59f56d14b07e
rs <yuiop1955@mail.ru>
2021-08-28 16:10:25 +0500

    feat(updates): update hook middleware

---
+++ telegram/updates/hook/hook.go
@@ -0,0 +1,33 @@
+package hook
+
+import (
+	"context"
+
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/bin"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/tg"
+)
+
+// UpdateHook middleware is called on each tg.UpdatesClass method result.
+//
+// Function is called before invoker return. Returned error will be wrapped
+// and returned as InvokeRaw result.
+type UpdateHook func(ctx context.Context, u tg.UpdatesClass) error
+
+// Handle implements telegram.Middleware.
+func (h UpdateHook) Handle(next tg.Invoker) telegram.InvokeFunc {
+	return func(ctx context.Context, input bin.Encoder, output bin.Decoder) error {
+		if err := next.Invoke(ctx, input, output); err != nil {
+			return err
+		}
+		if u, ok := output.(*tg.UpdatesBox); ok {
+			if err := h(ctx, u.Updates); err != nil {
+				return xerrors.Errorf("hook: %w", err)
+			}
+		}
+
+		return nil
+	}
+}
