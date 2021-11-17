098ab8e3aae88566a7a011aacbabfbb298514565
tdakkota <tanc13@yandex.ru>
2021-11-17 17:01:05 +0300

    feat: generate tdlib API

--- td.go
+++ td.go
@@ -3,3 +3,5 @@ package td
 
 //go:generate go run github.com/gotd/td/cmd/gotdgen --doc "https://core.telegram.org/" --clean --server --handlers --mapping --slices --package tg --target tg --schema _schema/telegram.tl
 //go:generate go run github.com/gotd/td/cmd/gotdgen --doc "https://core.telegram.org/" --clean --package e2e --target tg/e2e --schema _schema/encrypted.tl
+
+//go:generate go run github.com/gotd/td/cmd/gotdgen --clean --package tdapi --target tdapi --schema _schema/tdapi.tl
