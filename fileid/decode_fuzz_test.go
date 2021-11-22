451d6751e99703643bcd2c5642721eb176f3780d
tdakkota <tanc13@yandex.ru>
2021-11-22 13:17:20 +0300

    test(fileid): add decoder fuzzing

--- fileid/decode_fuzz_test.go
+++ fileid/decode_fuzz_test.go
@@ -1,1 +1,18 @@
 package fileid
+
+import (
+	"testing"
+)
+
+func FuzzDecodeFileID(f *testing.F) {
+	for _, input := range testData {
+		f.Add(input)
+	}
+
+	f.Fuzz(func(t *testing.T, fileID string) {
+		_, err := DecodeFileID(fileID)
+		if err != nil {
+			t.Skip(err)
+		}
+	})
+}
--- fileid/rle.go
+++ fileid/rle.go
@@ -1,1 +1,40 @@
 package fileid
+
+import "bytes"
+
+func rleEncode(s []byte) (r []byte) {
+	var count byte
+	for _, cur := range s {
+		if cur == 0 {
+			count++
+			continue
+		}
+
+		if count > 0 {
+			r = append(r, 0, count)
+			count = 0
+		}
+		r = append(r, cur)
+	}
+	if count > 0 {
+		r = append(r, 0, count)
+		count = 0
+	}
+
+	return r
+}
+
+func rleDecode(s []byte) (r []byte) {
+	var last []byte
+	for _, cur := range s {
+		if string(last) == string(rune(0)) {
+			r = append(r, bytes.Repeat(last, int(cur))...)
+			last = nil
+		} else {
+			r = append(r, last...)
+			last = []byte{cur}
+		}
+	}
+	r = append(r, last...)
+	return r
+}
