90ab9ab5d2d7fea21a25c9e610114f55eca75231
tdakkota <tanc13@yandex.ru>
2021-11-23 11:30:27 +0300

    chore(jsontd): pass by value instead of using pointer

--- jsontd/encoder.go
+++ jsontd/encoder.go
@@ -11,76 +11,76 @@ import (
 
 // Encoder is a simple wrapper around jx.Encoder to conform TL type system.
 type Encoder struct {
-	jx.Encoder
+	*jx.Encoder
 }
 
 // PutID serializes given typeID.
-func (b *Encoder) PutID(typeID string) {
-	b.Encoder.FieldStart("@type")
+func (b Encoder) PutID(typeID string) {
+	b.Encoder.FieldStart(TypeField)
 	b.Encoder.Str(typeID)
 }
 
 // PutInt serializes v as signed 32-bit integer.
-func (b *Encoder) PutInt(v int) {
+func (b Encoder) PutInt(v int) {
 	b.Encoder.Int(v)
 }
 
 // PutBool serializes boolean.
-func (b *Encoder) PutBool(v bool) {
+func (b Encoder) PutBool(v bool) {
 	b.Encoder.Bool(v)
 }
 
 // PutUint16 serializes unsigned 16-bit integer.
-func (b *Encoder) PutUint16(v uint16) {
+func (b Encoder) PutUint16(v uint16) {
 	b.Encoder.Uint32(uint32(v))
 }
 
 // PutInt32 serializes signed 32-bit integer.
-func (b *Encoder) PutInt32(v int32) {
+func (b Encoder) PutInt32(v int32) {
 	b.Encoder.Int32(v)
 }
 
 // PutUint32 serializes unsigned 32-bit integer.
-func (b *Encoder) PutUint32(v uint32) {
+func (b Encoder) PutUint32(v uint32) {
 	b.Encoder.Uint32(v)
 }
 
 // PutLong serializes v as signed integer.
-func (b *Encoder) PutLong(v int64) {
+func (b Encoder) PutLong(v int64) {
 	b.Encoder.Int64(v)
 }
 
 // PutUint64 serializes v as unsigned 64-bit integer.
-func (b *Encoder) PutUint64(v uint64) {
+func (b Encoder) PutUint64(v uint64) {
 	b.Encoder.Uint64(v)
 }
 
 // PutDouble serializes v as 64-bit floating point.
-func (b *Encoder) PutDouble(v float64) {
+func (b Encoder) PutDouble(v float64) {
 	b.Encoder.Float64(v)
 }
 
 // PutInt128 serializes v as 128-bit signed integer.
-func (b *Encoder) PutInt128(v bin.Int128) {
+func (b Encoder) PutInt128(v bin.Int128) {
 	// FIXME(tdakkota): neither TDLib API not Telegram API has no Int128/Int256 fields
 	// 	so this encoding may incorrect.
 	b.Encoder.Str(hex.EncodeToString(v[:]))
 }
 
 // PutInt256 serializes v as 256-bit signed integer.
-func (b *Encoder) PutInt256(v bin.Int256) {
+func (b Encoder) PutInt256(v bin.Int256) {
 	// FIXME(tdakkota): neither TDLib API not Telegram API has no Int128/Int256 fields
 	// 	so this encoding may incorrect.
 	b.Encoder.Str(hex.EncodeToString(v[:]))
 }
 
 // PutString serializes bare string.
-func (b *Encoder) PutString(s string) {
+func (b Encoder) PutString(s string) {
 	b.Encoder.Str(s)
 }
 
 // PutBytes serializes bare byte string.
-func (b *Encoder) PutBytes(v []byte) {
+func (b Encoder) PutBytes(v []byte) {
 	// See https://core.telegram.org/tdlib/docs/td__json__client_8h.html
 	//
 	// ... fields of bytes type are base64 encoded and then stored as String ...
---
+++ jsontd/jsontd.go
@@ -0,0 +1,5 @@
+// Package jsontd contains some helpers to use jx with TL objects.
+package jsontd
+
+// TypeField is a type field name.
+const TypeField = "@type"
