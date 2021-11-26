5845da9aeda215a0d757e55bd3965d2f61db84c3
Aleksandr Razumov <ernado@ya.ru>
2021-11-26 20:23:42 +0300

    test(coverage): optimize profile.out

--- go.coverage.sh
+++ go.coverage.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-go test -v -coverpkg=./... -coverprofile=profile.out ./...
-go tool cover -func profile.out
+go test -v -coverpkg=./... -coverprofile=profile_all.out ./...
+
+# Filter most generated code.
+# Reduces size from 864M to 29M.
+grep -v -P 'tl_.+_gen\.go' profile_all.out > profile.out
+rm profile_all.out
