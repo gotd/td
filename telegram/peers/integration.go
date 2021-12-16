cbf5056d8563386ccb06f856dfe4f2bb2a756f28
tdakkota <tanc13@yandex.ru>
2021-12-16 15:01:03 +0300

    feat(peers): add UpdateHook

---
+++ telegram/peers/integration.go
@@ -0,0 +1,71 @@
+package peers
+
+import (
+	"context"
+
+	"go.uber.org/multierr"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message/entity"
+	"github.com/gotd/td/tg"
+)
+
+// SetChannelAccessHash implements updates.ChannelAccessHasher.
+func (m *Manager) SetChannelAccessHash(userID, channelID, accessHash int64) error {
+	myID, ok := m.myID()
+	if !ok || myID != userID {
+		return nil
+	}
+	return m.storage.Save(context.TODO(), Key{
+		Prefix: channelPrefix,
+		ID:     channelID,
+	}, Value{
+		AccessHash: accessHash,
+	})
+}
+
+// GetChannelAccessHash implements updates.ChannelAccessHasher.
+func (m *Manager) GetChannelAccessHash(userID, channelID int64) (accessHash int64, found bool, err error) {
+	myID, ok := m.myID()
+	if !ok || myID != userID {
+		return 0, false, nil
+	}
+	v, found, err := m.storage.Find(context.TODO(), Key{
+		Prefix: channelPrefix,
+		ID:     channelID,
+	})
+	return v.AccessHash, found, err
+}
+
+// UpdateHook returns update middleware hook for collecting entities.
+func (m *Manager) UpdateHook(next telegram.UpdateHandler)  telegram.UpdateHandler {
+	f := func(ctx context.Context, u tg.UpdatesClass) error {
+		var (
+			users []tg.UserClass
+			chats []tg.ChatClass
+		)
+		switch u := u.(type) {
+		case *tg.UpdatesCombined:
+			users = u.GetUsers()
+			chats = u.GetChats()
+		case *tg.Updates:
+			users = u.GetUsers()
+			chats = u.GetChats()
+		}
+		applyErr := m.Apply(ctx, users, chats)
+		handleErr := next.Handle(ctx, u)
+		return multierr.Append(handleErr, applyErr)
+	}
+	return telegram.UpdateHandlerFunc(f)
+}
+
+// UserResolveHook creates entity.UserResolver attached to this Manager.
+func (m *Manager) UserResolveHook(ctx context.Context) entity.UserResolver {
+	return func(id int64) (tg.InputUserClass, error) {
+		r, err := m.getUser(ctx, &tg.InputUser{UserID: id})
+		if err != nil {
+			return nil, err
+		}
+		return r.AsInput(), nil
+	}
+}
