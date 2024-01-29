60eae8e799eb63a7fc8ab91eb48330c91b2a40f9
sadfun <job@sadfun.org>
2024-01-29 16:52:14 +0100

    fix: per-user locks on authorization transfer

--- telegram/transfer.go
+++ telegram/transfer.go
@@ -2,11 +2,15 @@ package telegram
 
 import (
 	"context"
-
 	"github.com/go-faster/errors"
 
 	"github.com/gotd/td/tg"
+
+	"sync"
 )
+
+var authorizationTransferLocks = make(map[int64]*sync.Mutex)
+var authorizationTransferLocksMutex = sync.Mutex{}
 
 func (c *Client) exportAuth(ctx context.Context, dcID int) (*tg.AuthExportedAuthorization, error) {
 	export, err := c.tg.AuthExportAuthorization(ctx, dcID)
@@ -20,6 +24,22 @@ func (c *Client) exportAuth(ctx context.Context, dcID int) (*tg.AuthExportedAuth
 // transfer exports current authorization and imports it to another DC.
 // See https://core.telegram.org/api/datacenter#authorization-transfer.
 func (c *Client) transfer(ctx context.Context, to *tg.Client, dc int) (tg.AuthAuthorizationClass, error) {
+	u, err := c.Self(context.Background())
+	if err != nil {
+		return nil, errors.Wrap(err, "get self")
+	}
+
+	authorizationTransferLocksMutex.Lock()
+	mx, ok := authorizationTransferLocks[u.ID]
+	if !ok {
+		mx = &sync.Mutex{}
+		authorizationTransferLocks[u.ID] = mx
+	}
+	authorizationTransferLocksMutex.Unlock()
+
+	mx.Lock()
+	defer mx.Unlock()
+
 	auth, err := c.exportAuth(ctx, dc)
 	if err != nil {
 		return nil, errors.Wrapf(err, "export to %d", dc)
