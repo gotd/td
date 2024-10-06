aaa7ad2e14ddc6f5e19dfa37511cd66289c4826a
Aleksandr Razumov <ernado@go-faster.org>
2024-10-06 23:57:27 +0300

    fix: update message helpers after API change
    Report API changed in backward incompatible way.

--- telegram/message/report.go
+++ telegram/message/report.go
@@ -22,58 +22,23 @@ func (b *ReportBuilder) Message(msg string) *ReportBuilder {
 	return b
 }
 
-func (b *ReportBuilder) send(ctx context.Context, reason tg.ReportReasonClass) (bool, error) {
+func (b *ReportBuilder) send(ctx context.Context, option []byte) (tg.ReportResultClass, error) {
 	p, err := b.peer(ctx)
 	if err != nil {
-		return false, errors.Wrap(err, "peer")
+		return nil, errors.Wrap(err, "peer")
 	}
 
 	return b.sender.report(ctx, &tg.MessagesReportRequest{
 		Peer:    p,
 		ID:      b.ids,
-		Reason:  reason,
+		Option:  option,
 		Message: b.message,
 	})
 }
 
-// Spam sends report for spam.
-func (b *ReportBuilder) Spam(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonSpam{})
-}
-
-// Violence sends report for violence.
-func (b *ReportBuilder) Violence(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonViolence{})
-}
-
-// Pornography sends report for pornography.
-func (b *ReportBuilder) Pornography(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonPornography{})
-}
-
-// ChildAbuse sends report for child abuse.
-func (b *ReportBuilder) ChildAbuse(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonChildAbuse{})
-}
-
-// Other sends report for other.
-func (b *ReportBuilder) Other(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonOther{})
-}
-
-// Copyright sends report for copyrighted content.
-func (b *ReportBuilder) Copyright(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonCopyright{})
-}
-
-// GeoIrrelevant sends report for irrelevant geogroup.
-func (b *ReportBuilder) GeoIrrelevant(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonGeoIrrelevant{})
-}
-
-// Fake sends report for fake.
-func (b *ReportBuilder) Fake(ctx context.Context) (bool, error) {
-	return b.send(ctx, &tg.InputReportReasonFake{})
+// Option sends report with provided option field.
+func (b *ReportBuilder) Option(ctx context.Context, option []byte) (tg.ReportResultClass, error) {
+	return b.send(ctx, option)
 }
 
 // Report reports messages in a chat for violation of Telegram's Terms of Service.
