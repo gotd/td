9d5d1f31ea5022d9798d84ccce15de2e91ba6baa
expary <259320579+expary@users.noreply.github.com>
2026-06-03 12:01:04 +0800

    fix(proto): validate plaintext message length before allocation

--- proto/unencrypted_message_test.go
+++ proto/unencrypted_message_test.go
@@ -1,6 +1,7 @@
 package proto
 
 import (
+	"io"
 	"testing"
 
 	"github.com/gotd/td/bin"
@@ -23,4 +24,33 @@ func TestUnencryptedMessage_Encode(t *testing.T) {
 	}
 	require.Equal(t, d, decoded)
 	require.Zero(t, b.Len(), "buffer should be consumed")
+}
+
+func TestUnencryptedMessage_DecodeInvalidDataLength(t *testing.T) {
+	t.Run("Negative", func(t *testing.T) {
+		b := new(bin.Buffer)
+		b.PutLong(0)
+		b.PutLong(1)
+		b.PutInt32(-1)
+
+		var decoded UnencryptedMessage
+		err := decoded.Decode(b)
+		require.Error(t, err)
+
+		var lengthErr *bin.InvalidLengthError
+		require.ErrorAs(t, err, &lengthErr)
+		require.Zero(t, decoded.MessageData)
+	})
+
+	t.Run("ExceedsRemainingBuffer", func(t *testing.T) {
+		b := new(bin.Buffer)
+		b.PutLong(0)
+		b.PutLong(1)
+		b.PutInt32(1024)
+
+		var decoded UnencryptedMessage
+		err := decoded.Decode(b)
+		require.ErrorIs(t, err, io.ErrUnexpectedEOF)
+		require.Zero(t, decoded.MessageData)
+	})
 }
